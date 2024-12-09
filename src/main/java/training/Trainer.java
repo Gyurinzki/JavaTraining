eeb0e69a92860fcdcb5ac2cf00e723a03ca8309b
T360-v5-JAVA-s02 <laszlo.gyure@bmw.hu>
2024-12-09 14:06:21 +0100

    1

--- src/main/java/training/Trainer.java
+++ src/main/java/training/Trainer.java
@@ -3,6 +3,30 @@ package training;
 public class Trainer {
 
     String name;
-    int YearOfBirth;
+    private int YearOfBirth;
+
+    public String getName() {
+        return name;
     }
+
+    public  String getNameAndYearofBirth() {
+        return name + " " + YearOfBirth;
+    }
+
+    public void changeName(String newName) {
+        name = newName;
+    }
+
+    public int getYearOfBirth() {
+        return YearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        YearOfBirth = yearOfBirth;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
 
--- src/main/java/training/TrainerMain.java
+++ src/main/java/training/TrainerMain.java
@@ -1,5 +1,6 @@
 package training;
 
+import javax.security.auth.login.AccountNotFoundException;
 import java.sql.SQLOutput;
 
 public class TrainerMain {
@@ -7,13 +8,23 @@ public class TrainerMain {
     public static void main(String[] args) {
         Trainer trainer = new Trainer();
         trainer.name = "John Doe";
-        trainer.YearOfBirth = 1980;
+        trainer.setYearOfBirth(1980);
         System.out.println(trainer.name);
-        System.out.println(trainer.YearOfBirth);
+        System.out.println(trainer.getYearOfBirth());
 
         Trainer anotherTrainer = new Trainer();
         anotherTrainer.name = "Jack Doe";
+        anotherTrainer.setYearOfBirth(1981);
         System.out.println(anotherTrainer.name);
+
+        System.out.println(anotherTrainer.getName());
+        System.out.println(anotherTrainer.getNameAndYearofBirth());
+
+        anotherTrainer.changeName("Joe Doe");
+        System.out.println(anotherTrainer.getName());
+
+        anotherTrainer.setName("Jack Doe");
+        System.out.println(anotherTrainer.getName());
     }
 
 }
